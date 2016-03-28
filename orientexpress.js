a4c08b29e3d2b57156403cb51739aeacf22fe395
Paul Horner <paulhorner79@gmail.com>
2016-03-28 15:08:05 +0100

    4 space indentation
    Why did it have 3 spaces?  Something weird going on with my editor?

--- orientexpress.js
+++ orientexpress.js
@@ -1,82 +1,83 @@
-var OrientExpress = function (auto) {
-   if (auto === undefined) {
-       auto = true;
-   }
-   this.auto        = Boolean(auto);
-   this.landscape   = false;
-   this.portrait    = false;
-   this.orientation = 'square';
-   this.width       = 0;
-   this.height      = 0;
 
-   this.init();
+var OrientExpress = function (auto) {
+    if (auto === undefined) {
+        auto = true;
+    }
+    this.auto        = Boolean(auto);
+    this.landscape   = false;
+    this.portrait    = false;
+    this.orientation = "square";
+    this.width       = 0;
+    this.height      = 0;
+
+    this.init();
 };
 
 OrientExpress.prototype.init = function () {
-   var orientExpress = this;
-   /**
-    * When a page is reloaded set the orientation.
-    */
-   if (this.auto) {
-       window.onresize = function() {
-           orientExpress.setOrientation();
-       };
-       var resize = window.onresize;
-   }
-   this.setOrientation();
+    var orientExpress = this;
+    /**
+     * When a page is reloaded set the orientation.
+     */
+    if (this.auto) {
+        window.onresize = function() {
+            orientExpress.setOrientation();
+        };
+        var resize = window.onresize;
+    }
+    this.setOrientation();
 };
 
 /**
-* Sets the orientation.  Called when the window is resized or the object
-* is initialised.
-*/
+ * Sets the orientation.  Called when the window is resized or the object
+ * is initialised.
+ */
 OrientExpress.prototype.setOrientation = function() {
 
-   this.height = Math.min(window.innerHeight, window.outerHeight);
-   this.width = Math.min(window.innerWidth, window.outerWidth);
+    this.height = Math.min(window.innerHeight, window.outerHeight);
+    this.width  = Math.min(window.innerWidth, window.outerWidth);
 
-   if (this.width > this.height) {
-       this.landscape   = true;
-       this.portrait    = false;
-       this.orientation = 'landscape';
-   }
-   else if (this.height > this.width) {
-       this.landscape   = false;
-       this.portrait    = true;
-       this.orientation = 'portrait';
-   }
-   else {
-       this.landscape   = false;
-       this.portrait    = false;
-       this.orientation = 'square';
-   }
+    if (this.width > this.height) {
+        this.landscape   = true;
+        this.portrait    = false;
+        this.orientation = "landscape";
+    }
+    else if (this.height > this.width) {
+        this.landscape   = false;
+        this.portrait    = true;
+        this.orientation = "portrait";
+    }
+    else {
+        this.landscape   = false;
+        this.portrait    = false;
+        this.orientation = "square";
+    }
 };
 
 /**
-* Gets the orientation.
-*
-* @return {string} `portrait`, `landscape`, or `square`.
-*/
+ * Gets the orientation.
+ *
+ * @return {string} `portrait`, `landscape`, or `square`.
+ */
 OrientExpress.prototype.getOrientation = function() {
-   return this.orientation;
+    return this.orientation;
 };
 
 /**
-* Returns true if the page is landscape.  Will return false if it is
-* portrait or square.
-*
-* @return {Boolean}
-*/
+ * Returns true if the page is landscape.  Will return false if it is
+ * portrait or square.
+ *
+ * @return {Boolean}
+ */
 OrientExpress.prototype.isLandscape = function() {
-   return this.landscape;
+    return this.landscape;
 };
 
 /**
-* Returns true if the page is portrait.  Will return false if it is
-* landscape or square.
-*
-* @return {Boolean}
-*/
+ * Returns true if the page is portrait.  Will return false if it is
+ * landscape or square.
+ *
+ * @return {Boolean}
+ */
 OrientExpress.prototype.isPortrait = function() {
-   return this.portrait;
+    return this.portrait;
 };
